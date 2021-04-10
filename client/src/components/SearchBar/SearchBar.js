7809e5108f6e54994dc0b1738c195459a92fe2ae
Samya Fiki <crystalgrey@Samyas-MBP.fios-router.home>
2021-04-09 20:48:35 -0400

    Added starter code with folders and corresponding files.

---
+++ client/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./style.css"
+
+
+function SearchBar(){
+    return(
+        <div>
+            <button>Search</button>
+        </div>
+
+    )
+}
+export default SearchBar;
---
+++ client/src/components/SearchInput/SearchInput.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./style.css"
+
+
+function SearchInput(){
+    return(
+        <div>
+            <input>
+            
+            </input>
+        </div>
+
+    )
+}
+export default SearchInput;
